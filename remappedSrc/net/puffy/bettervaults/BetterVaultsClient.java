3be8708104c739ce892cab6faf7f0782dc8d2734
trpipher <trpipher@gmail.com>
2023-03-22 23:36:45 -0400

    Initial commit

---
+++ remappedSrc/net/puffy/bettervaults/BetterVaultsClient.java
@@ -0,0 +1,10 @@
+package net.puffy.bettervaults;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterVaultsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
